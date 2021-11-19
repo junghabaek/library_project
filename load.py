95318ac6c2f38080bffe6dfaca02f649e29f2aa4
Jung Ha Baek <junghabaek@gmail.com>
2021-11-19 14:37:29 +0900

    test

---
+++ load.py
@@ -0,0 +1,23 @@
+import pymysql
+import csv
+
+
+db_connection = pymysql.connect(
+    user='root',
+    passwd='614614',
+    host='127.0.0.1',
+    db='library',
+    charset='utf8'
+)
+
+con = db_connection
+cur = con.cursor(pymysql.cursors.DictCursor)
+
+with open('books.csv', 'r', 'utf-8') as datas:
+    records = csv.DictReader(datas)
+    result = [(w['title'], w['publisher'], w['author'], w['publication_date'],
+               w['pages'], str(w['isbn']), w['description'], w['link']) for w in records]
+
+cur.executemany("insert into book(title, publisher, author, publication_date, pages, isbn, description, link) values(%s, %s, %s, %s, %s, %s, %s, %s);", result)
+con.commit()
+con.close()
--- models.py
+++ models.py
@@ -4,59 +4,68 @@ from datetime import datetime
 
 
 class User(db.Model):
-    __tablename__="user"
+    __tablename__ = "user"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    user_id=db.Column(db.String(100), nullable=False)
-    password=db.Column(db.String(200), nullable=False)
-    user_name=db.Column(db.String(50), nullable=False)
+    id = db.Column(db.Integer, primary_key=True,
+                   autoincrement=True, nullable=False)
+    user_id = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    user_name = db.Column(db.String(50), nullable=False)
 
-    def __init__ (self, user_id, password, user_name):
-        self.user_id=user_id
-        self.password=password
-        self.user_name=user_name
-    
+    def __init__(self, user_id, password, user_name):
+        self.user_id = user_id
+        self.password = password
+        self.user_name = user_name
+
+
 class Book (db.Model):
-    __tablename__='book'
+    __tablename__ = 'book'
 
-    id=db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    title=db.Column(db.String(100), nullable=False)
-    publisher=db.Column(db.String(100), nullable=False)
-    author=db.Column(db.String(100), nullable=False)
-    publication_date=db.Column(db.DateTime, nullable=False)
-    pages=db.Column(db.Integer, nullable=False)
-    isbn=db.Column(db.String(15), nullable=False)
-    description=db.Column(db.Text(), nullable=False)
-    link=db.Column(db.String(200), nullable=False)
-    img_url=db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True,
+                   nullable=False, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publication_date = db.Column(db.DateTime, nullable=False)
+    pages = db.Column(db.Integer, nullable=False)
+    isbn = db.Column(db.String(15), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+    link = db.Column(db.String(200), nullable=False)
+    img_url = db.Column(db.String(200), nullable=True)
+    stock = db.Column(db.Integer, nullable=True)
+
 
 class Comment (db.Model):
-    __tablename__='comment'
+    __tablename__ = 'comment'
 
-    id = db.Column(db.Integer, primary_key=True,              nullable=False, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True,
+                   nullable=False, autoincrement=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    #foreign key 임 referencing user_id
+    # foreign key 임 referencing user_id
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
 
     content = db.Column(db.Text(), nullable=False)
     time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    rating= db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
 
 class Rented (db.Model):
-    __tablename__='rented'
-    id = db.Column(db.Integer, primary_key=True,              nullable=False, autoincrement=True)
-    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    book_id=db.Column(db.Integer, db.ForeignKey('book.id'))
+    __tablename__ = 'rented'
+    id = db.Column(db.Integer, primary_key=True,
+                   nullable=False, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
     #isReturned=db.Column(db.Boolean, default=False)
-    rented_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    rented_time = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
     returned_time = db.Column(db.DateTime, nullable=True, default=None)
 
 
 class Reservation (db.Model):
-    __tablename__='reservation'
+    __tablename__ = 'reservation'
     id = db.Column(db.Integer, primary_key=True,
-    nullable=False, autoincrement=True)
+                   nullable=False, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
-    isReserved= db.Column(db.Boolean, default=True)
+    isReserved = db.Column(db.Boolean, default=True)
